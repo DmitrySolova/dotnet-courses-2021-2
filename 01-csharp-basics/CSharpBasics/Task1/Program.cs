03d1d9080c9424316eb24fc65329fa2829187354
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-22 19:49:25 +0300

    Add 2 tasks

---
+++ 01-csharp-basics/CSharpBasics/Task1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Строки сторон, которые будем проверять на наличие корректного ввода
+
+            string inputA = "";
+            string inputB = "";
+
+            // Числа сторон и площади, с которыми будем работать
+
+            int parsedA;
+            int parsedB;
+            int square;
+
+            // Переменная-флаг, показывающий что ввод значения корректен
+
+            bool incorrectInput = true;
+
+            // Сообщение о некорректном значении ввода
+
+            string wrongInputMassage = "Ошибка! Ввести можно только целые числа > 0";
+
+            Console.WriteLine("Привет! Эта программа для расчета площади прямоугольника со сторонами A и B");
+            Console.WriteLine("ВНИМАНИЕ! Нельзя вводить некорректные значения сторон: 0, отрицательные или нецелые числа, строки");
+
+            do
+            {
+                Console.Write("Ввведи сторону A: ");
+                inputA = Console.ReadLine();
+
+                if (int.TryParse(inputA, out parsedA) == false)
+                {
+                    incorrectInput = true;
+                }
+                else
+                {
+                    if (parsedA <= 0)
+                    {
+                        Console.WriteLine(wrongInputMassage);
+                        incorrectInput = true;
+                    }
+                    else
+					{
+                        incorrectInput = false;
+                    }
+                }
+            } while (incorrectInput);
+
+            do
+            {
+                Console.Write("Ввведи сторону B: ");
+                inputB = Console.ReadLine();
+
+                if (int.TryParse(inputB, out parsedB) == false)
+                {
+                    incorrectInput = true;
+                }
+                else
+                {
+                    if (parsedB <= 0)
+                    {
+                        Console.WriteLine(wrongInputMassage);
+                        incorrectInput = true;
+                    }
+                    else
+					{
+                        square = parsedA * parsedB;
+                        Console.WriteLine("Площадь прямоугольника со сторонами (A;B) ({0},{1}) = {2}", parsedA, parsedB, square);
+                        incorrectInput = false;
+                    }
+                }
+            } while (incorrectInput);
+
+        }
+    }
+}
---
+++ 01-csharp-basics/CSharpBasics/Task2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Task2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            // Строка, которую будем парсить на проверку правильности ввода N
+
+            string inputN;
+
+            // Количество строк и символов
+
+            int parsedN;
+
+            // Переменная-флаг, показывающий что ввод значения корректен
+
+            bool incorrectInput = false;
+
+            // Сообщение о некорректном значении ввода
+
+            string wrongInputMassage = "Ошибка! Ввести можно только целые числа > 0";
+
+            // Строка, которая должна вывести необходимое количество символов в зависимости от числа N
+
+            string result = "";
+
+            Console.WriteLine("Привет! Эта программа для отрисовки половинки елочки");
+			Console.WriteLine("Число N - это количество строк в елочке и количество символов в ней");
+			Console.WriteLine("Например: N = 3 даст следующую картинку");
+
+			Console.WriteLine("");
+			Console.WriteLine("*");
+			Console.WriteLine("**");
+			Console.WriteLine("***");
+			Console.WriteLine("");
+
+			Console.WriteLine("ВНИМАНИЕ! Нельзя вводить некорректные значения N: 0, отрицательные или нецелые числа, строки");
+
+            do
+            {
+                Console.Write("Ввведи число N: ");
+                inputN = Console.ReadLine();
+
+                if (int.TryParse(inputN, out parsedN) == false)
+                {
+                    incorrectInput = true;
+                }
+                else
+                {
+                    if (parsedN <= 0)
+                    {
+                        Console.WriteLine(wrongInputMassage);
+                        incorrectInput = true;
+                    }
+                    else
+                    {
+                        incorrectInput = false;
+                        for (int i = 0; i < parsedN; i++)
+						{
+                            result += "*";
+                            Console.WriteLine(result);
+                        }
+                    }
+                }
+            } while (incorrectInput);
+        }
+	}
+}
