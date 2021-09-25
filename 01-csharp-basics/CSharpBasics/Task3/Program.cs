0008259c60b249dd913af51a1002688b9aad7b4c
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-25 15:47:26 +0300

    Update Program.cs

--- 01-csharp-basics/CSharpBasics/Task3/Program.cs
+++ 01-csharp-basics/CSharpBasics/Task3/Program.cs
@@ -6,23 +6,72 @@ namespace Task3
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Высота елочки");
-            int width = int.Parse(Console.ReadLine());
-			for (int i = 1; i < width + 1; i++)
+			// Строка, которую будем парсить на проверку правильности ввода N
+
+			string inputN;
+
+			// Высота елочки
+
+			int parsedN;
+
+			// Переменная-флаг, показывающий что ввод значения корректен
+
+			bool incorrectInput = false;
+
+			// Сообщение о некорректном значении ввода
+
+			string wrongInputMassage = "Ошибка! Ввести можно только целые числа > 0";
+
+			Console.WriteLine("Привет! Эта программа для отрисовки елочки");
+			Console.WriteLine("Число N - это высота елочки");
+			Console.WriteLine("Например: N = 3 даст следующую картинку");
+
+			Console.WriteLine("");
+			Console.WriteLine("  *");
+			Console.WriteLine(" ***");
+			Console.WriteLine("*****");
+			Console.WriteLine("");
+
+			Console.WriteLine("ВНИМАНИЕ! Нельзя вводить некорректные значения N: 0, отрицательные или нецелые числа, строки");
+
+			do
 			{
-				for (int n = i; n < width; n++)
+				Console.Write("Ввведи число N: ");
+				inputN = Console.ReadLine();
+
+				if (int.TryParse(inputN, out parsedN) == false)
 				{
-					Console.Write(" ");
+					Console.WriteLine(wrongInputMassage);
+					incorrectInput = true;
 				}
-				for (int k = 0; k != i * 2 - 1; k++)
+				else
 				{
-					Console.Write("*");
+					if (parsedN <= 0)
+					{
+						Console.WriteLine(wrongInputMassage);
+						incorrectInput = true;
+					}
+					else
+					{
+						incorrectInput = false;
+						for (int i = 1; i < parsedN + 1; i++)
+						{
+							for (int n = i; n < parsedN; n++)
+							{
+								Console.Write(" ");
+							}
+							for (int k = 0; k != i * 2 - 1; k++)
+							{
+								Console.Write("*");
+							}
+							if (i != parsedN)
+							{
+								Console.WriteLine();
+							}
+						}
+					}
 				}
-				if (i != width)
-                {
-					Console.WriteLine();
-                }
-			}
+			} while (incorrectInput);
         }
 	}
 }
