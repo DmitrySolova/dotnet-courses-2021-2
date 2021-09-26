53b85940be08ebed4510b5e90cc668a96b73e775
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-26 13:42:06 +0300

    Update Program.cs

--- 02-arrays-and-methods/ArraysAndMethods/Task1/Program.cs
+++ 02-arrays-and-methods/ArraysAndMethods/Task1/Program.cs
@@ -17,19 +17,27 @@ namespace Task1
             return arr;
         }
 
+        //метод обмена элементов
+        static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
         static void SortAndGetMinAndMaxValues(int[] array, out int min, out int max)
         {
-            for (int i = 0; i < array.Length; i++)
+            for (var i = 1; i < array.Length; i++)
             {
-                for (int j = 0; j < array.Length - 1; j++)
+                var key = array[i];
+                var j = i;
+                while ((j > 1) && (array[j - 1] > key))
                 {
-                    if (array[j] > array[j + 1])
-                    {
-                        int t = array[j + 1];
-                        array[j + 1] = array[j];
-                        array[j] = t;
-                    }
+                    Swap(ref array[j - 1], ref array[j]);
+                    j--;
                 }
+
+                array[j] = key;
             }
 
             min = array[0];
