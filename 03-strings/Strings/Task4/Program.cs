fe460512efc5d0a6661821e994d8a80f28b3335f
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-28 02:17:58 +0300

    Загрузил 4 таск

--- 03-strings/Strings/Task4/Program.cs
+++ 03-strings/Strings/Task4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace Task4
 {
@@ -6,6 +8,33 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            int N = 100;
+
+            Stopwatch stopWatchStringBuilder = new Stopwatch();
+            Stopwatch stopWatchString = new Stopwatch();
+
+            stopWatchString.Start();
+            for (int i = 0; i < N; i++)
+            {
+                str += "*";
+            }
+            stopWatchString.Stop();
+
+            stopWatchStringBuilder.Start();
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("*");
+            }
+            stopWatchStringBuilder.Stop();
+
+            TimeSpan tsString = stopWatchString.Elapsed;
+            TimeSpan tsStringBuilder = stopWatchStringBuilder.Elapsed;
+
+            Console.WriteLine("String: " + tsString.Milliseconds);
+            Console.WriteLine("StringBuilder: " + tsStringBuilder.Milliseconds);
 
         }
     }
