802eac6745774eb8d92054e226c105c2d8654687
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-28 00:14:18 +0300

    Добавил 2 таск

--- 03-strings/Strings/Task2/Program.cs
+++ 03-strings/Strings/Task2/Program.cs
@@ -4,9 +4,34 @@ namespace Task2
 {
     class Program
     {
+        static string DoublingCharactersOfString(string firstString, string secondString)
+        {
+            string finalString = "";
+
+            foreach (char ch in firstString)
+                if (secondString.Contains(ch))
+                {
+                    finalString += ch;
+                    finalString += ch;
+                }
+                else
+                {
+                    finalString += ch;
+                }
+
+            return finalString;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите первую строку: ");
+            string firstString = Console.ReadLine();
+
+            Console.Write("Введите вторую строку: ");
+            string secondString = Console.ReadLine();
+
+            string finalString = DoublingCharactersOfString(firstString, secondString);
+
+            Console.WriteLine(finalString);
         }
     }
 }
