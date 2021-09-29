445eb1c6067d2b111ae79916f226fa136688a732
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-29 22:22:09 +0300

    Добавил 2 таск

--- 04-reference-types/ReferenceTypes/Task2/Program.cs
+++ 04-reference-types/ReferenceTypes/Task2/Program.cs
@@ -2,11 +2,54 @@
 
 namespace Task2
 {
+    class Round
+    {
+        int _radius;
+        int _x;
+        int _y;
+        double _circumference;
+        double _area;
+
+        public Round (int radius, int x, int y)
+        {
+            _radius = radius;
+            _x = x;
+            _y = y;
+        }
+
+        public int Radius
+        {
+            get => _radius;
+        }
+
+        public int X
+        {
+            get => _x;
+        }
+
+        public int Y
+        {
+            get => _y;
+        }
+
+        public double Circumference
+        {
+            get => Math.Round(2 * Math.PI * _radius, 2);
+        }
+
+        public double Area
+        {
+            get => Math.Round(Math.PI * _radius * _radius, 2);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Round round = new Round(5, 0, 0);
+            Console.WriteLine(round.Area);
+            Console.WriteLine(round.Circumference);
         }
     }
 }
--- 04-reference-types/ReferenceTypes/Task4/Program.cs
+++ 04-reference-types/ReferenceTypes/Task4/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Task4
 {
+    
     class Program
     {
         static void Main(string[] args)
