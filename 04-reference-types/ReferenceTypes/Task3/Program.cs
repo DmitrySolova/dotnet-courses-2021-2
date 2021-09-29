7a32960e7ff90eef77a67c2c230fd1d49340099f
DmitrySolova <55030300+DmitrySolova@users.noreply.github.com>
2021-09-29 22:58:42 +0300

    Добавил 3 таск

--- 04-reference-types/ReferenceTypes/Task3/Program.cs
+++ 04-reference-types/ReferenceTypes/Task3/Program.cs
@@ -2,11 +2,56 @@
 
 namespace Task3
 {
+    class Triangle
+    {
+        int _a;
+        int _b;
+        int _c;
+
+        public Triangle (int a, int b, int c)
+        {
+
+            if (!((a + b >= c) & (b + c >= a) & (a + c) >= b))
+            {
+                throw new ArgumentOutOfRangeException($" Некорректный ввод - треугольник с такими сторонами не существует");
+            }
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public int A
+        {
+            get => _a;
+        }
+        public int B
+        {
+            get => _b;
+        }
+        public int C
+        {
+            get => _c;
+        }
+
+        public double GetArea()
+        {
+            double p = (_a + _b + _c) / 2.0;
+            double area = Math.Sqrt(p * (p - _a) * (p - _b) * (p - _c));
+            return Math.Round(area, 2);
+        }
+
+        public int GetPerimeter()
+        {
+            return _a + _b + _c;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Triangle triangle = new Triangle(3, 5, 7);
+            Console.WriteLine(triangle.GetArea());
+            Console.WriteLine(triangle.GetPerimeter());
         }
     }
 }
--- 04-reference-types/ReferenceTypes/Task4/Program.cs
+++ 04-reference-types/ReferenceTypes/Task4/Program.cs
@@ -7,7 +7,18 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int a = 1;
+            int b = 2;
+            int c = 5;
+
+            string rez;
+
+            if (!((a + b > c) & (b + c > a) & (a + c) > b))
+            {
+                throw new ArgumentOutOfRangeException($" Некорректный ввод - треугольник с такими сторонами не существует");
+            }
+
         }
     }
 }
